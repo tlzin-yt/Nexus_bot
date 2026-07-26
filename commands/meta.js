29e24d0e9e6a7b70f050ea03ec95a1e6e47d9613
TL-STORE <tuliofonseca00@gmail.com>
2026-07-26 16:17:42 -0300

    Update meta.js

--- commands/meta.js
+++ commands/meta.js
@@ -19,7 +19,7 @@ module.exports = {
         const falta = alvo - atual;
 
         const embed = new EmbedBuilder()
-            .setColor('#FFA500')
+            .setColor(''#002B49')
             .setTitle('🎯 Calculadora de Meta - GTA Online')
             .addFields(
                 { name: '🏦 Dinheiro Atual', value: `$${atual.toLocaleString('pt-BR')}`, inline: true },
