a09d0bfdea0f3746de4ca926360c49e91a4b336a
TL-STORE <tuliofonseca00@gmail.com>
2026-07-26 17:02:54 -0300

    Create vehicles.js

---
+++ database/vehicles.js
@@ -0,0 +1,41 @@
+const vehicles = {
+    "krieger": {
+        name: "Benefactor Krieger",
+        class: "Super",
+        price: "$2.875.000",
+        topSpeed: "127.25 mph (204.79 km/h)",
+        lapTime: "0:59.135",
+        source: "Legendary Motorsport",
+        image: "https://static.wikia.nocookie.net/gtawiki/images/images/Krieger.png"
+    },
+    "emerus": {
+        name: "Progen Emerus",
+        class: "Super",
+        price: "$2.750.000",
+        topSpeed: "127.25 mph (204.79 km/h)",
+        lapTime: "0:59.327",
+        source: "Legendary Motorsport",
+        image: "https://static.wikia.nocookie.net/gtawiki/images/images/Emerus.png"
+    },
+    "pariah": {
+        name: "Ocelot Pariah",
+        class: "Sports",
+        price: "$1.420.000",
+        topSpeed: "136.00 mph (218.87 km/h)",
+        lapTime: "1:00.814",
+        source: "Legendary Motorsport",
+        image: "https://static.wikia.nocookie.net/gtawiki/images/images/Pariah.png"
+    },
+    "buffalo stx": {
+        name: "Bravado Buffalo STX",
+        class: "Muscle",
+        price: "$2.150.000 ($1.612.500 na Promoção)",
+        topSpeed: "126.50 mph (203.58 km/h)",
+        lapTime: "1:05.250",
+        source: "Southern San Andreas Super Autos",
+        image: "https://static.wikia.nocookie.net/gtawiki/images/images/BuffaloSTX.png"
+    }
+};
+
+module.exports = vehicles;
+
